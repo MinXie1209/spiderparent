d016a38df45ab6e69bf7495f2c9b2793dbb8ecac
xyj1209 <15817061252@163.com>
2018-12-01 00:24:21 +0800

    封装返回的资源

---
+++ spider-core/src/main/java/top/myjnxj/selector/Html.java
@@ -0,0 +1,17 @@
+package top.myjnxj.selector;
+
+/**
+ * @ClassName Html
+ * @Description 封装返回的资源
+ * @Author 江南小俊
+ * @Date 2018/11/30 21:34
+ * @Version 1.0.0
+ **/
+public class Html {
+    public Html() {
+    }
+
+    public Html(String rawText, String url) {
+
+    }
+}
---
+++ spider-core/src/main/java/top/myjnxj/selector/Json.java
@@ -0,0 +1,17 @@
+package top.myjnxj.selector;
+
+/**
+ * @ClassName Json
+ * @Description TODO
+ * @Author 江南小俊
+ * @Date 2018/11/30 21:55
+ * @Version 1.0.0
+ **/
+public class Json {
+    public Json(String rawText) {
+
+    }
+
+    public Json() {
+    }
+}
